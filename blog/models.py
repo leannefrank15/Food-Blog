5f79346f8ceea2af75ab04f824efaf8f6782fcc2
leannefrank <leannfrank@gmail.com>
2021-08-27 17:55:06 +0530

    uploaded image files and displayed them using database

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from django.core.validators import MinLengthValidator
+from django.db.models.deletion import CASCADE
 
 # Create your models here.
+
+class Tag(models.Model):
+  caption = models.CharField(max_length=20)
+
+  def tagname(self):
+    return f"{self.caption}"
+
+  def __str__(self):
+    return self.tagname()
+
+
+class Author(models.Model):
+  firstname = models.CharField(max_length=50)
+  lastname = models.CharField(max_length=50)
+  email = models.EmailField()
+
+  def fullname(self):
+    return f"{self.firstname} {self.lastname}"
+
+  def __str__(self):
+    return self.fullname()
+  
+
+class Post(models.Model):
+  title=models.CharField(max_length=50)
+  excerpt = models.CharField(max_length=250)
+  image = models.ImageField(upload_to="posts", null=True)
+  date = models.DateField(auto_now=True)
+  slug = models.SlugField(unique=True)
+  content = models.TextField(validators=[MinLengthValidator(10)])
+  author = models.ForeignKey(Author, on_delete=models.SET_NULL, null=True, related_name="posts") #sets up one to many relationship
+  tags = models.ManyToManyField(Tag)
--- blog/views.py
+++ blog/views.py
@@ -1,90 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from datetime import date
-all_posts=[
-    {
-        "slug": "gluten-free-donuts",
-        "image": "donuts.jpg",
-        "author": "Leanne",
-        "date": date(2021,8,23),
-        "title": "Gluten-Free Cinnamon Donuts",
-        "excerpt" : "Donuts that are gluten-free AND low calorie??? You've got to try these!!",
-        "content" : """ 
-           Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem ratione, veniam 
-           maiores a vero eligendi odit accusantium incidunt esse excepturi sit beatae mollitia.
-           Ipsam dolorem mollitia, cupiditate ullam esse quia!
+from .models import Post
 
-           Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem ratione, veniam 
-           maiores a vero eligendi odit accusantium incidunt esse excepturi sit beatae mollitia.
-           Ipsam dolorem mollitia, cupiditate ullam esse quia!
-
-           Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem ratione, veniam 
-           maiores a vero eligendi odit accusantium incidunt esse excepturi sit beatae mollitia.
-           Ipsam dolorem mollitia, cupiditate ullam esse quia!
-        """
-    },
-    {
-        "slug": "keto-pizza",
-        "image": "pizza.jpg",
-        "author": "Leanne",
-        "date": date(2021, 8, 19),
-        "title": "Cauliflower Crust Keto Pizza",
-        "excerpt": "Delicious Keto Pizza which is low in carbs but a 10/10 in taste.",
-        "content": """
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-        """
-    },
-    {
-        "slug": "chocolate-chip-cookies",
-        "image": "cookies.jpg",
-        "author": "Leanne",
-        "date": date(2021, 8, 23),
-        "title": "Moist Chocolate-Chip Cookies",
-        "excerpt": "Make these cookies with ingredients available at home. Check it out!",
-        "content": """
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-        """
-    }
-]
 
 def get_date(post):
     return post['date']
 
 def starting_page(request):
-    sorted_posts=sorted(all_posts, key=get_date)
-    latest_posts=sorted_posts[-3:]
+    latest_posts = Post.objects.all().order_by("-date")[:3]
     return render(request, "blog/index.html", {
         "posts": latest_posts
     })
 
 
 def posts(request):
+    all_posts= Post.objects.all().order_by("-date")
     return render(request, "blog/all-posts.html", {
         "all_posts" : all_posts
     })
 
 
 def post_detail(request, slug): #imp
-    single_post = next(post for post in all_posts if post['slug']==slug)
+    single_post = get_object_or_404(Post, slug=slug)
     return render(request, "blog/post-detail.html",{
-        "single_post" : single_post
+        "single_post" : single_post,
+        "post_tags" : single_post.tags.all()
     })
